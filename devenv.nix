b3611083beff76732ea4d3d4ae3b2788a05d6f21
Andrei Jiroh Halili <ajhalili2006@andreijiroh.dev>
2025-01-29 15:27:37 +0800

    chore(nix): revert nixpkgs config to devenv based config setup
    well rip flake.nix for now at the moment
    
    Signed-off-by: Andrei Jiroh Halili <ajhalili2006@andreijiroh.dev>

--- devenv.nix
+++ devenv.nix
@@ -1,5 +1,16 @@
-{ pkgs, ... }:
+{ pkgs, prisma-utils, nixpkgs, ... }:
 
+#let
+#  prisma =
+#    (prisma-utils.lib.prisma-factory {
+#      inherit nixpkgs;
+#      prisma-fmt-hash = "sha256-4zsJv0PW8FkGfiiv/9g0y5xWNjmRWD8Q2l2blSSBY3s="; # just copy these hashes for now, and then change them when nix complains about the mismatch
+#      query-engine-hash = "sha256-6ILWB6ZmK4ac6SgAtqCkZKHbQANmcqpWO92U8CfkFzw=";
+#      libquery-engine-hash = "sha256-n9IimBruqpDJStlEbCJ8nsk8L9dDW95ug+gz9DHS1Lc=";
+#      schema-engine-hash = "sha256-j38xSXOBwAjIdIpbSTkFJijby6OGWCoAx+xZyms/34Q=";
+#    }).fromNpmLock
+#      ./package-lock.json; # <--- path to our package-lock.json file that contains the version of prisma-engines
+#in
 {
   # https://devenv.sh/reference/options/
   packages =  with pkgs; [
@@ -28,6 +39,8 @@
     PRISMA_QUERY_ENGINE_LIBRARY = "${pkgs.prisma-engines}/lib/libquery_engine.node";
     PRISMA_QUERY_ENGINE_BINARY = "${pkgs.prisma-engines}/bin/query-engine";
     PRISMA_SCHEMA_ENGINE_BINARY = "${pkgs.prisma-engines}/bin/schema-engine";
+    PRISMA_INTROSPECTION_ENGINE_BINARY = "${pkgs.prisma-engines}/bin/introspection-engine";
+	  PRISMA_FMT_BINARY = "${pkgs.prisma-engines}/bin/prisma-fmt";
   };
 
   devcontainer = {
@@ -36,4 +49,6 @@
       updateContentCommand = "echo \"info: skipping 'devenv test' per https://github.com/cachix/devenv/issues/1120 for now\"";
     };
   };
+
+  #enterShell = prisma.shellHook;
 }
